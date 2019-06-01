24be7e25134addd3d1d973c418a5beacd7011e5f
Cancain <eriksson.tomas@gmail.com>
2019-06-01 16:30:51 +0200

    completed lecture 283

--- redux-basics.js
+++ redux-basics.js
@@ -2,11 +2,23 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const initialState = {
-  couter: 0
+  counter: 0
 };
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -15,5 +27,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 //Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 
 //Suscription
